1477677e7efa5bffaaf6500dfe6524692fa7db14
Rafael Perez Castaño <alum.rperezc.2021@iesalixar.org>
2021-09-22 17:45:42 +0200

    Añado el adios mundo a hola mundo

--- src/main/java/U1/T1/hola_mundo.java
+++ src/main/java/U1/T1/hola_mundo.java
@@ -3,5 +3,6 @@ package U1.T1;
 public class hola_mundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Adios mundo");
     }
 }
