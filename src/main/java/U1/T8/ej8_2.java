a60ca41c0e4f5767a8cf82eefa4422a1b450d09f
Rafael Perez Castaño <alum.rperezc.2021@iesalixar.org>
2021-10-07 18:16:00 +0200

    8.2 casi terminado

--- src/main/java/U1/T8/ej8_2.java
+++ src/main/java/U1/T8/ej8_2.java
@@ -7,6 +7,28 @@ public class ej8_2 {
     Scanner teclado = new Scanner(System.in);
     System.out.println("Introduzca la edad del alumno: ");
     int edad = teclado.nextInt();
-    while (edad >= 0) {}
+    int sumaEdades = 0;
+    int numeroAlumnos = 0;
+    int alumnosMayores = 0;
+    int media = sumaEdades / numeroAlumnos;
+    while (edad >= 0) {
+      numeroAlumnos++;
+      sumaEdades = sumaEdades + edad;
+      if (edad >= 18) {
+        alumnosMayores++;
+      }
+      System.out.println("Introduce la edad de otro alumno: ");
+      edad = teclado.nextInt();
+    }
+    System.out.println(
+        "El numero de alumnos es de: "
+            + numeroAlumnos
+            + ". La suma de las edades es de: "
+            + sumaEdades
+            + ", la media de edad es de: "
+            + media
+            + " y hay "
+            + alumnosMayores
+            + " mayores de edad.");
   }
 }
