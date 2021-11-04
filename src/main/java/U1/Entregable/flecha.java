66bc8f340be8dafac990497f2d95739db0ba24f1
rafpercas <you@example.com>
2021-11-04 11:00:03 +0100

    flecha done

--- src/main/java/U1/Entregable/flecha.java
+++ src/main/java/U1/Entregable/flecha.java
@@ -13,7 +13,7 @@ public class flecha {
         }while(altura%2==0 || altura<3);
         for (int i = 0; i < altura; i++) {
             for (int j = 0; j <= altura+5; j++) {
-                if(j>altura/2-i && j>i-(altura/2) && j<=(altura/2)+1 || i==(altura/2) &&j>1&& j<altura+3){
+                if(j>altura/2-i && j>i-(altura/2) && j<=(altura/2)+1 || i==(altura/2) &&j>=altura/2&& j<=(altura/2)+1+5){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
