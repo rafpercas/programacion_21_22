8e68edc3038a6f9b82db503817fe5dcf9c5ee913
Rafael Perez Castaño <alum.rperezc.2021@iesalixar.org>
2021-11-29 17:10:31 +0100

    hasta 3-4-4 sin terminar

--- src/main/java/U3/T4/U3_T4_ej3.java
+++ src/main/java/U3/T4/U3_T4_ej3.java
@@ -1,5 +1,9 @@
 package U3.T4;
-// Un restaurante nos ha encargado una aplicación para colocar a los clientes ensus mesas. En una
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Un restaurante nos ha encargado una aplicación para colocar a los clientes en sus mesas. En una
 // mesa se pueden sentar de 0 (mesa vacía) a 4 comensales(mesa llena).
 //
 // Cuando llega un cliente se le pregunta cuántos son. De momento el programa no está preparado para
@@ -14,4 +18,47 @@ package U3.T4;
 //
 // Cada vez que se sientan nuevos clientes se debe mostrar el estado de las mesas. Los grupos no se
 // pueden romper aunque haya huecos sueltos suficientes.
-public class U3_T4_ej3 {}
+public class U3_T4_ej3 {
+  public static void main(String[] args) {
+    boolean haySitio = false;
+    Scanner teclado = new Scanner(System.in);
+    int clientes;
+    int[] mesas = new int[10];
+    for (int i = 0; i < mesas.length; i++) {
+      mesas[i] = i + 1;
+    }
+    int[] ocupacion = new int[10];
+    for (int i = 0; i < ocupacion.length; i++) {
+      ocupacion[i] = (int) (Math.random() * 5);
+    }
+    do {
+
+      do {
+        System.out.println("Cuantos son?");
+        clientes = teclado.nextInt();
+        if (clientes > 4) {
+          System.out.println(
+              "Lo siento, no admitimos grupos de 6, haga grupos de 4 personas como máximo e intente de nuevo");
+        }
+      } while (clientes > 4);
+
+      for (int i = 0; i < ocupacion.length; i++) {
+
+        if (ocupacion[i] < 4) {
+          if (ocupacion[i] + clientes < 5) {
+            System.out.println(ocupacion[i]);
+            ocupacion[i] = ocupacion[i] + clientes;
+            System.out.println("Mesa " + (i + 1) + " por favor.");
+            haySitio = true;
+            break;
+          } else {
+            haySitio = false;
+            System.out.println("No tenemos sitio.");
+          }
+        }
+      }
+      System.out.println(Arrays.toString(mesas));
+      System.out.println(Arrays.toString(ocupacion));
+    } while (haySitio = true);
+  }
+}
--- src/main/java/U3/T4/U3_T4_ej4.java
+++ src/main/java/U3/T4/U3_T4_ej4.java
@@ -1,6 +1,32 @@
 package U3.T4;
+
+import java.util.Arrays;
+
 // Escribe un programa que, dada una posición en un tablero de ajedrez, nos diga a qué casillas
 // podría saltar un alfil que se encuentra en esa posición. Como se indica en la figura, el alfil se
 // mueve siempre en diagonal. El tablero cuenta con64 casillas.
 // Las columnas se indican con las letras de la “a” a la “h” y las filas se indican del 1 al 8.
-public class U3_T4_ej4 {}
+public class U3_T4_ej4 {
+  public static void main(String[] args) {
+    String letras = "abcdefgh";
+    String[][] tablero = new String[8][8];
+    for (int i = 0; i < tablero.length; i++) {
+      for (int j = 0; j < tablero[i].length; j++) {
+        tablero[i][j] = letras.charAt(j) + String.valueOf(tablero.length - i);
+      }
+      for (String[] fila : tablero) {
+        System.out.println(Arrays.toString(fila));
+      }
+    }
+    String posicion = "d5";
+
+    for (int i = 0; i < tablero.length; i++) {
+      for (int j = 0; j < tablero[i].length; j++) {
+        if (posicion.equalsIgnoreCase(tablero[i][j])) {
+          /*          x = i;
+          y=j;  */
+        }
+      }
+    }
+  }
+}
