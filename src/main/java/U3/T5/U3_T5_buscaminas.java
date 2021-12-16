a75f0c84492f6ac26c3b46457871cf8365f8f864
rafpercas <you@example.com>
2021-12-16 11:04:21 +0100

    buscaminas corregido

--- src/main/java/U3/T5/U3_T5_buscaminas.java
+++ src/main/java/U3/T5/U3_T5_buscaminas.java
@@ -30,12 +30,45 @@ public class U3_T5_buscaminas {
 
     String [] bomba = {"-","x"};
 
-    String [][] matriz = new String [n][n];
-    for (int i = 0; i < matriz.length; i++) {
-      for (int j = 0; j < matriz[i].length; j++) {
-        matriz[i][j]=bomba[(int) (Math.random()*2)];
+    String [][] tablero = new String [n][n];
+    for (int i = 0; i < tablero.length; i++) {
+      for (int j = 0; j < tablero[i].length; j++) {
+        tablero[i][j]=bomba[(int) (Math.random()*2)];
       }
-      System.out.println(Arrays.toString(matriz[i]));
     }
+    for (String[] fila : tablero) {
+      System.out.println(Arrays.toString(fila));
+    }
+    int x, y;
+    do {
+
+      System.out.println("Introduzca la fila de la posición:");
+      x = teclado.nextInt();
+
+      System.out.println("Introduzca la columna de la posición:");
+      y = teclado.nextInt();
+
+      int numBombas = 0;
+
+      for (int i = (x - 1); i < (x + 2); i++) {
+        for (int j = (y - 1); j < (y + 2); j++)
+          if (i >= 0 && i < tablero.length && j >= 0 && j < tablero.length) {
+            if (tablero[i][j].equalsIgnoreCase(bomba[1])) {
+              numBombas++;
+            }
+          }
+      }
+
+        for (String[] fila : tablero) {
+          System.out.println(Arrays.toString(fila));
+        }
+
+      System.out.println(
+              "La posición " + x + "," + y + " tiene " + numBombas + " bombas en los alrededores");
+
+    } while (!tablero[x][y].equalsIgnoreCase(bomba[1]));
+
+    System.out.println("La posición " + x + "," + y + " tiene una bomba, está muerto");
   }
-}
+  }
+
